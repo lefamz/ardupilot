9cb97233026c89dd7efd3a28b0d94772fd9a34c6
Michal Zajacik <michal.zajacik@agents.fel.cvut.cz>
2014-07-02 12:10:09 +0200

    Update to version 3.1.5

--- ArduCopter/APM_Config.h
+++ ArduCopter/APM_Config.h
@@ -23,11 +23,11 @@
 #define LOGGING_ENABLED       ENABLED            // disable dataflash logging to save 11K of flash space
 #define GPS_PROTOCOL         GPS_PROTOCOL_UBLOX  // hard code GPS to Ublox to save 8k of flash
 //#define GPS_PROTOCOL          GPS_PROTOCOL_MTK19  // hard cdoe GPS to Mediatek to save 10k of flash
-#define MOUNT                 DISABLED            // disable the camera gimbal to save 8K of flash space
+#define MOUNT                 ENABLED            // disable the camera gimbal to save 8K of flash space
 #define AUTOTUNE              DISABLED            // disable the auto tune functionality to save 7k of flash
 #define OPTFLOW               DISABLED            // disable optical flow sensor to save 5K of flash space
 #define AC_FENCE              DISABLED            // disable fence to save 2k of flash
-#define CAMERA                DISABLED            // disable camera trigger to save 1k of flash
+#define CAMERA                DISABLED           // disable camera trigger to save 1k of flash
 #define CONFIG_SONAR          ENABLED            // disable sonar to save 1k of flash
 
 // features below are disabled by default
